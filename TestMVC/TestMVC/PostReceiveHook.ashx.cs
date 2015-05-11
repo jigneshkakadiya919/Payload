1ffad6a97d0440b6596ac02e041741c5207426a7
jigneshkakadiya919 <jigneshkakadiya919@gmail.com>
2015-05-11 11:13:46 +0530

    15

--- TestMVC/TestMVC/PostReceiveHook.ashx.cs
+++ TestMVC/TestMVC/PostReceiveHook.ashx.cs
@@ -49,128 +49,76 @@ namespace TestMVC
         /// <param name="command">string command</param>
         /// <returns>string, as output of the command.</returns>
         /// 
-         public void ExecuteCommandSync(object command)
-         {
-             System.Diagnostics.ProcessStartInfo psi =
-  new System.Diagnostics.ProcessStartInfo(@"C:\Users\Jignesh\Desktop\Jeff\test2.bat");
-             psi.RedirectStandardOutput = true;
-             psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
-             psi.UseShellExecute = false;
-             System.Diagnostics.Process listFiles;
-             listFiles = System.Diagnostics.Process.Start(psi);
-             System.IO.StreamReader myOutput = listFiles.StandardOutput;
-             listFiles.WaitForExit(2000);
-             //if (listFiles.HasExited == true)
-             //{
-                 string output = myOutput.ReadToEnd();
-                 string re = output;
-           //  }
-             listFiles.Close();
-         }
-       
+        public void ExecuteCommandSync(object command)
+        {
+            System.Diagnostics.ProcessStartInfo psi =
+ new System.Diagnostics.ProcessStartInfo(@"C:\Users\Jignesh\Desktop\Jeff\test2.bat");           
+            psi.RedirectStandardOutput = true;
+            psi.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            psi.UseShellExecute = false;
+            System.Diagnostics.Process listFiles;
+            listFiles = System.Diagnostics.Process.Start(psi);
+            System.IO.StreamReader myOutput = listFiles.StandardOutput;
+            listFiles.WaitForExit(2000);
+            listFiles.Kill();
+            if (listFiles.HasExited == true)
+            {
+                string output = myOutput.ReadToEnd();
+                string re = output;
+                
+            }
+
+        }
+
 
         //public void ExecuteCommandSync(object command)
         //{
+
         //    try
         //    {
 
-        //        string strFilePath = "C:\\Users\\Jignesh\\Desktop\\Jeff\\test2.bat";
+        //        //Log.Log(command.ToString());
+        //        //Log.Log("begin deploy");
+        //        // create the ProcessStartInfo using "cmd" as the program to be run,
+        //        // and "/c " as the parameters.
+        //        // Incidentally, /c tells cmd that we want it to execute the command that follows,
+        //        // and then exit.                
+        //        var start = new System.Diagnostics.ProcessStartInfo("cmd.exe ", "" + command);
 
-        //        // Create the ProcessInfo object
-        //        System.Diagnostics.ProcessStartInfo psi =
-        //                new System.Diagnostics.ProcessStartInfo("cmd.exe");
-        //        psi.UseShellExecute = false;
-        //        psi.RedirectStandardOutput = true;
-        //        psi.RedirectStandardInput = true;
-        //        psi.RedirectStandardError = true;
+        //        // The following commands are needed to redirect the standard output.
+        //        // This means that it will be redirected to the Process.StandardOutput StreamReader.
+        //        start.RedirectStandardOutput = true;
+        //        start.UseShellExecute = false;
+        //        // Do not create the black window.
+        //        start.CreateNoWindow = true;
+        //        // Now we create a process, assign its ProcessStartInfo and start it
+        //        System.Diagnostics.Process proc = new System.Diagnostics.Process();
+        //        proc.StartInfo = start;
+        //        proc.Start();
+        //        proc.WaitForExit(5000);
+        //        //var output = new List<string>();
 
-        //        // Start the process
-        //        System.Diagnostics.Process proc =
-        //                   System.Diagnostics.Process.Start(psi);
+        //        //while (proc.StandardOutput.Peek() > -1)
+        //        //{
+        //        //    output.Add(proc.StandardOutput.ReadLine());
+        //        //}
 
-        //        // Open the batch file for reading
-        //        System.IO.StreamReader strm1 =
-        //                   System.IO.File.OpenText(strFilePath);
-        //        System.IO.StreamReader strm = proc.StandardError;
-        //        // Attach the output for reading
-        //        System.IO.StreamReader sOut = proc.StandardOutput;
-
-        //        // Attach the in for writing
-        //        System.IO.StreamWriter sIn = proc.StandardInput;
-
-        //        // Write each line of the batch file to standard input
-        //        /*while(strm.Peek() != -1)
-        //        {
-        //            sIn.WriteLine(strm.ReadLine());
-        //        }*/
-        //        sIn.WriteLine("");
-        //        strm.Close();
-
-        //        // Exit CMD.EXE
-        //        string stEchoFmt = "# {0} run successfully. Exiting";
-
-        //        sIn.WriteLine(String.Format(stEchoFmt, strFilePath));
-        //        sIn.WriteLine("EXIT");
-        //        proc.WaitForExit();
-        //        // Close the process
-        //        proc.Close();
-
-        //        // Read the sOut to a string.
-        //        string results = sOut.ReadToEnd().Trim();
-
-        //        // Close the io Streams;
-        //        sIn.Close();
-        //        sOut.Close();
-
-        //        // Write out the results.
-        //        //string fmtStdOut = "<font face=courier size=0>{0}</font>";
-        //        //this.Response.Write("<br>");
-        //        //this.Response.Write("<br>");
-        //        //this.Response.Write("<br>");
-        //        //this.Response.Write(String.Format(fmtStdOut,
-        //        //   results.Replace(System.Environment.NewLine, "<br>")));
-
-        //       // //Log.Log(command.ToString());
-        //       // //Log.Log("begin deploy");
-        //       // // create the ProcessStartInfo using "cmd" as the program to be run,
-        //       // // and "/c " as the parameters.
-        //       // // Incidentally, /c tells cmd that we want it to execute the command that follows,
-        //       // // and then exit.                
-        //       // var start = new System.Diagnostics.ProcessStartInfo("cmd.exe ", "" + command);
-
-        //       // // The following commands are needed to redirect the standard output.
-        //       // // This means that it will be redirected to the Process.StandardOutput StreamReader.
-        //       // start.RedirectStandardOutput = true;
-        //       // start.UseShellExecute = false;
-        //       // // Do not create the black window.
-        //       // start.CreateNoWindow = true;
-        //       // // Now we create a process, assign its ProcessStartInfo and start it
-        //       // System.Diagnostics.Process proc = new System.Diagnostics.Process();
-        //       // proc.StartInfo = start;
-        //       // proc.Start();
-        //       // var output = new List<string>();
-
-        //       // while (proc.StandardOutput.Peek() > -1)
-        //       // {
-        //       //     output.Add(proc.StandardOutput.ReadLine());
-        //       // }
-
-        //       // while (proc.StandardError.Peek() > -1)
-        //       // {
-        //       //     output.Add(proc.StandardError.ReadLine());
-        //       // }
-        //       // proc.WaitForExit();
-        //       //// proc.WaitForExit();
-        //       // // Get the output into a string
-        //       // string result= "";
-        //       // //Log.Log(result); // Display the command output.
-        //       // //Log.Log("end deploy");
+        //        //while (proc.StandardError.Peek() > -1)
+        //        //{
+        //        //    output.Add(proc.StandardError.ReadLine());
+        //        //}
+              
+        //        // proc.WaitForExit();
+        //        // Get the output into a string
+        //        string result = proc.StandardOutput.ReadToEnd();
+        //        //Log.Log(result); // Display the command output.
+        //        //Log.Log("end deploy");
 
         //    }
         //    catch (Exception exp)
         //    {
         //        // Log the exception
-        //       // Log.LogError(exp.Message);
+        //        // Log.LogError(exp.Message);
         //    }
         //}
         /// <summary>
